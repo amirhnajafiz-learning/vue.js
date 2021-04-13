76137a80c841f7f04ef046f59e7102e1a35321b4
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2021-04-13 21:34:52 +0430

    click counter done

--- Vue School/01.Components/Click Counter/app.js
+++ Vue School/01.Components/Click Counter/app.js
@@ -1,5 +1,5 @@
 Vue.component('click-counter', {
-    template: '<button @click="count++">You clicked me {{ count }} times</button>',
+    template: '#click-counter-template',
     data () {
         return {
             count: 0
@@ -9,4 +9,20 @@ Vue.component('click-counter', {
 
 new Vue({
     el: '#app',
+})
+
+
+new Vue({
+    el: '#counters',
+    components: {
+        'click-counter' : {
+            template : "<button @click=\"count++\">{{count}}</button>",
+            data: function ()
+            {
+                return {
+                    count : 0
+                }
+            }
+        }
+    }
 })
