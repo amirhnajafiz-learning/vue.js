46a1aef7416fdbeccd8699ed213b11b76ecc22f4
9831065 <najafizadeh21@gmail.com>
2021-04-16 21:44:27 +0430

    Md first template added

---
+++ 05. Medical View Project/MedicalView/src/components/PersonalForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="personalFormContainer">
+        <h2>Please fill out this form about your personal information.</h2>
+        <hr />
+        <span>
+          <label for="user-firstname">First name:</label>
+          <input id="user-firstname" type="text" v-model="firstName" placeholder="Peter" />
+        </span>
+        <span>
+          <label for="user-lastname">Last name:</label>
+          <input id="user-lastname" type="text" v-model="lastName" placeholder="Griffen" />
+        </span>
+        <span>
+          <label for="user-age">Age:</label>
+          <input id="user-age" type="number" v-model="age" />
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonalForm',
+  data: () => ({
+    firstName: undefined,
+    lastName: undefined,
+    age: undefined,
+    nationalId: undefined,
+    bloodType: undefined
+  })
+}
+</script>
--- 05. Medical View Project/MedicalView/src/main.js
+++ 05. Medical View Project/MedicalView/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import App from './App'
 import router from './router'
+import PersonalForm from '@/components/PersonalForm'
 
 Vue.config.productionTip = false
 
@@ -8,5 +8,5 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  render: h => h(PersonalForm)
 })
