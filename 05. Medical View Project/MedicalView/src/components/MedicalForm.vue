29d4c0317d3aeed4f1c980892e64b596bcccbd12
9831065 <najafizadeh21@gmail.com>
2021-04-17 10:41:24 +0430

    second comp add

--- 05. Medical View Project/MedicalView/src/components/MedicalForm.vue
+++ 05. Medical View Project/MedicalView/src/components/MedicalForm.vue
@@ -2,13 +2,34 @@
     <div class="medicalFormContainer">
         <h2>Please give us information about your medical status.</h2>
         <hr />
-        <span>
-          <label for="user-alive">Alive ?</label>
-          <input id="user-alive" type="checkbox" v-model="dead" />
+        <span class="user-input-row">
+          <label for="user-blood-type">Blood type:</label>
+          <select id="user-blood-type" v-model="blood_type">
+            <option id="0" value="A+">A+</option>
+            <option id="0" value="A-">A-</option>
+            <option id="0" value="B+">B+</option>
+            <option id="0" value="B-">B-</option>
+            <option id="0" value="Ab+">Ab+</option>
+            <option id="0" value="O+">O+</option>
+            <option id="0" value="O-">O-</option>
+          </select>
         </span>
-        <span>
-          <label for="user-drugs">Drugs:</label>
-          <input id="user-drugs" type="text" v-model="drugs" />
+        <span class="user-input-row">
+          <label for="covid19">Do you have Corona Virus ?</label>
+          <input id="covid19" type="checkbox" v-model="has_covid19" />
+        </span>
+        <span class="user-input-row">
+          <span>Drugs:</span>
+          <label for="asprin">Asprin</label>
+          <input id="asprin" type="checkbox" v-model="drugs" />
+          <label for="zinc">Zinc</label>
+          <input id="zinc" type="checkbox" v-model="drugs" />
+          <label for="metadol">Metadol</label>
+          <input id="metadol" type="checkbox" v-model="drugs" />
+        </span>
+        <span class="user-input-row">
+          <lable for="comments">Any comments ?</lable><br />
+          <textarea id="comments" v-model="comment"></textarea>
         </span>
     </div>
 </template>
@@ -17,8 +38,21 @@
 export default {
   name: 'MedicalForm',
   data: () => ({
-    drugs: undefined,
-    dead: undefined
+    blood_type: undefined,
+    has_covid19: undefined,
+    drugs: [],
+    comment: undefined
   })
 }
 </script>
+
+<style scoped>
+  .user-input-row {
+    display: block;
+    padding: 20px;
+    margin: 10px auto;
+    border: 1px solid black;
+    width: 40%;
+    border-radius: 5px;
+  }
+</style>
