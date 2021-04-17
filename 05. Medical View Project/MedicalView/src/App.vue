639f2a4b9541bea6a2571ba3a0e0058c71f8242b
9831065 <najafizadeh21@gmail.com>
2021-04-17 13:34:46 +0430

    Nested fixed

---
+++ 05. Medical View Project/MedicalView/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <PersonalForm></PersonalForm>
+        <MedicalForm></MedicalForm>
+    </div>
+</template>
+
+<script>
+import PersonalForm from '@/components/PersonalForm.vue'
+import MedicalForm from '@/components/MedicalForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PersonalForm,
+    MedicalForm
+  }
+}
+
+</script>
--- 05. Medical View Project/MedicalView/src/main.js
+++ 05. Medical View Project/MedicalView/src/main.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
-import PersonalForm from '@/components/PersonalForm'
-import MedicalForm from '@/components/MedicalForm'
+import App from './App'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(PersonalForm)
-})
-
-new Vue({
-  el: '#mdf',
-  render: h => h(MedicalForm)
+  render: h => h(App)
 })
