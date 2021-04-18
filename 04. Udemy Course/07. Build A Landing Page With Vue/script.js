4ca59605711eb1bdcddfd932c4540d4618f560da
9831065 <najafizadeh21@gmail.com>
2021-04-18 12:39:52 +0430

    Lp Project plain base

--- 04. Udemy Course/07. Build A Landing Page With Vue/script.js
+++ 04. Udemy Course/07. Build A Landing Page With Vue/script.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
                 {
                     title: "Angular",
                     link: {
-                        url: "https://angular.oi",
+                        url: "https://angular.io",
                         src: "https://vuejs.org/images/logo.png"
                     },
                     text: "Complete component based user interface framework. Includes most core features. More common at startups."
@@ -30,3 +30,25 @@ const app = Vue.createApp({
         }
     }
 })
+
+app.component("cards-component", {
+    props: ["title", "link", "text"],
+    template: `
+        <div class="card">
+
+            <h2>{{ title }}</h2>
+
+            <img v-bind:src="link.src" v-bind:alt="title" />
+
+            <p>{{ text }}</p>
+
+            <a :href="link.url" target="blank">
+                <button>
+                    {{ title }} website
+                </button>
+            </a>
+        </div>
+    `
+})
+
+app.mount("#app")
