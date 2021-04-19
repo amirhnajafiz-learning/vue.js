fb30a704ea06685b7623c77f7c1759ec7069906b
9831065 <najafizadeh21@gmail.com>
2021-04-19 09:50:52 +0430

    todo list time style

--- 04. Udemy Course/09. Interactive To Do List/script.js
+++ 04. Udemy Course/09. Interactive To Do List/script.js
@@ -14,16 +14,26 @@ const app = Vue.createApp({
                 id: this.nextTaskId++,
                 title: this.newTask,
                 priority: this.currentPriority,
-                createTime: new Date()
+                createTime: new Date(),
             })
             this.newTask = ""
             this.currentPriority = 0
+        },
+        generateDate(config) {
+            return config.getDate() + "/" + config.getMonth() + "/" + config.getFullYear() + " " + config.getHours() + ":" + config.getMinutes()
         },
         sortTasks(raise) {
             this.tasks.sort((a,b) => (a.createTime > b.createTime) ? raise : ((b.createTime > a.createTime) ? -1 * raise : 0))
         },
         sortByPrio(raise) {
             this.tasks.sort((a,b) => (a.priority > b.priority) ? raise : ((b.priority > a.priority) ? -1 * raise : 0))
+        },
+        getAllTasks() {
+            var a = document.createElement('a')
+            var file = new Blob([JSON.stringify(this.tasks, null, 2)], {type: 'text/plain'})
+            a.href = URL.createObjectURL(file)
+            a.download = "tasks.txt"
+            a.click()
         }
     }
 })
