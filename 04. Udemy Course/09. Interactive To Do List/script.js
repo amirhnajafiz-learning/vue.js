a2776900bd5627e26bb10affaeeee663a9ed92bf
9831065 <najafizadeh21@gmail.com>
2021-04-19 09:36:13 +0430

    todo list complete js

--- 04. Udemy Course/09. Interactive To Do List/script.js
+++ 04. Udemy Course/09. Interactive To Do List/script.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     data() {
         return {
             newTask: "",
+            currentPriority: 0,
             tasks: [],
             nextTaskId: 0
         }
@@ -11,21 +12,32 @@ const app = Vue.createApp({
         addNewTask() {
             this.tasks.push({
                 id: this.nextTaskId++,
-                title: this.newTask
+                title: this.newTask,
+                priority: this.currentPriority,
+                createTime: new Date()
             })
             this.newTask = ""
+            this.currentPriority = 0
+        },
+        sortTasks(raise) {
+            this.tasks.sort((a,b) => (a.createTime > b.createTime) ? raise : ((b.createTime > a.createTime) ? -1 * raise : 0))
+        },
+        sortByPrio(raise) {
+            this.tasks.sort((a,b) => (a.priority > b.priority) ? raise : ((b.priority > a.priority) ? -1 * raise : 0))
         }
     }
 })
 
 app.component("todo-item", {
     template: `
-        <li>
-            {{ title }}
-            <button @click="$emit('remove')"> Remove Task </button>
-        </li>
+        <tr>
+            <td>{{ title }}</td>
+            <td>{{ time }}</td>
+            <td>{{ priority }}</td>
+            <td><button @click="$emit('remove')"> Remove Task </button></td>
+        </tr>
     `,
-    props: ["title"]
+    props: ["title", "time", "priority"]
 })
 
 app.mount("#app")
