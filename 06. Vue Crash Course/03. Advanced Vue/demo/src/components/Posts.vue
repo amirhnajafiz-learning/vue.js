07c8b234f87bc84ca262f7518e699187b3626145
9831065 <najafizadeh21@gmail.com>
2021-04-25 11:31:26 +0430

    build nested components with bootstrap register

--- 06. Vue Crash Course/03. Advanced Vue/demo/src/components/Posts.vue
+++ 06. Vue Crash Course/03. Advanced Vue/demo/src/components/Posts.vue
@@ -1,22 +1,42 @@
 <template>
-    <div>
-        <p>Hello</p>
-    </div>
+  <div class="container">
+    <table class="table table-dark mt-4">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Title</th>
+          <th scope="col">Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td>{{ post.id }}</td>
+          <td>{{ post.title }}</td>
+          <td>{{ post.body }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 
 <script>
 export default {
-  name: 'PostExample',
+  name: "PostExample",
   data() {
-      return {
-
-      }
-  }
-}
+    return {
+      // This way of creating object of data for a component
+      posts: [
+        { id: 1, title: "post1", body: "aigohgagihaoga" },
+        { id: 2, title: "post2", body: "ahgiahghsihdd" },
+        { id: 3, title: "post3", body: "ahsihgishg" },
+        { id: 4, title: "post4", body: "ahhdoowtyow" },
+      ],
+    };
+  },
+};
 </script>
 
 
 <style>
-
 </style>
